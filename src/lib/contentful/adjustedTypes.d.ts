e930c96d34f2c95011f4e64d18fe9a7f8c675654
Joe Choi <joechoi.newyork@gmail.com>
2024-07-24 14:15:09 -0400

    bynder

--- src/lib/contentful/adjustedTypes.d.ts
+++ src/lib/contentful/adjustedTypes.d.ts
@@ -16,10 +16,47 @@ type Document = {
     json: any;
 };
 
-export interface BlogDetailProps extends Omit<IBlogPostFields, "details" | "heroImage">, IBlogPost {
+type BynderAsset = {
+    archive: number;
+    brandId: string;
+    copyright: string | null;
+    dateCreated: string;
+    dateModified: string;
+    datePublished: string;
+    description: string | null;
+    extension: string[];
+    fileSize: number;
+    height: number;
+    id: string;
+    isPublic: number;
+    limited: number;
+    name: string;
+    orientation: string;
+    original: string;
+    thumbnails: {
+        webimage: string;
+        thul: string;
+        original: string;
+        mini: string;
+        transformBaseUrl: string;
+        Social: string;
+    };
+    type: string;
+    watermarked: number;
+    width: number;
+    videoPreviewURLs: string[];
+    tags: string[];
+    textMetaproperties: string[];
+    src: string;
+};
+
+export interface BlogDetailProps
+    extends Omit<IBlogPostFields, "details" | "heroImage" | "carousel">,
+        IBlogPost {
     sys: IBlogPost["sys"] & Sys; // extend with &
     details: Document; // override with omit
     heroImage: Asset; // override with omit
+    carousel: BynderAsset; // override with omit
     relatedBlogsCollection?: {
         items: BlogDetailProps[]; // add
     };
