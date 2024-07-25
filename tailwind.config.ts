2c692ec3a5f83cb37ef5a9398d0432ca298c865d
Joe Choi <joechoi.newyork@gmail.com>
2024-07-25 07:43:29 -0400

    bynder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,19 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
-            }
-        }
-    },
-    plugins: []
+
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+    // daisyUI config (optional - here are the default values)
+    daisyui: {
+        themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: "dark", // name of one of the included themes for dark mode
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+        themeRoot: ":root" // The element that receives theme color CSS variables
+    }
 };
 export default config;
