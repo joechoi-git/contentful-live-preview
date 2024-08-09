714e1a3f6b6b8540446b6ccc1cbacb867e8315c5
Joe Choi <joechoi.newyork@gmail.com>
2024-08-08 23:10:59 -0400

    in progress

--- locales/client.ts
+++ locales/client.ts
@@ -1,7 +1,8 @@
 "use client";
 import { createI18nClient } from "next-international/client";
 
-export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
-    en: () => import("./en"),
-    es: () => import("./es")
-});
+export const { useI18n, useScopedI18n, I18nProviderClient, useCurrentLocale, useChangeLocale } =
+    createI18nClient({
+        en: () => import("./en"),
+        es: () => import("./es")
+    });
--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useChangeLocale, useCurrentLocale } from "../../locales/client";
 
 interface LanguageSwitcherProps {
     className?: string;
@@ -8,6 +9,17 @@ const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ className }) => {
     const [language, setLanguage] = useState<string>(
         process.env.NEXT_PUBLIC_CONTENTFUL_LOCALE || "en-US"
     );
+
+    let currentLocale = "en";
+    let changeLocale = undefined;
+    try {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        currentLocale = useCurrentLocale();
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        changeLocale = useChangeLocale();
+    } catch (error) {
+        console.log((error as Error).message);
+    }
 
     useEffect(() => {
         const savedLanguage = localStorage.getItem("language");
@@ -20,17 +32,27 @@ const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ className }) => {
         const newLanguage = event.target.value;
         setLanguage(newLanguage);
         localStorage.setItem("language", newLanguage);
+        if (changeLocale) {
+            if (newLanguage === "en-US") {
+                changeLocale("en");
+            } else {
+                changeLocale("es");
+            }
+        }
     };
 
     return (
-        <select
-            value={language}
-            onChange={handleLanguageChange}
-            className={`rounded border-solid border-4 p-2 border-accent bg-primary text-primary-content ${className}`}
-        >
-            <option value="en-US">English</option>
-            <option value="es-US">Spanish</option>
-        </select>
+        <>
+            <p>Locale: {currentLocale}</p>
+            <select
+                value={language}
+                onChange={handleLanguageChange}
+                className={`rounded border-solid border-4 p-2 border-accent bg-primary text-primary-content ${className}`}
+            >
+                <option value="en-US">English</option>
+                <option value="es-US">Spanish</option>
+            </select>
+        </>
     );
 };
 
